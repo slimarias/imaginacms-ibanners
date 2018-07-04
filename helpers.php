21ecaa7137033c2e3ed62c6474de4137b2788086
Carlos Devia <carlosdevia@imagincolombia.com>
2018-07-04 11:23:52 -0500

    +Fix: helpers.php table queries wrong

--- helpers.php
+++ helpers.php
@@ -9,9 +9,10 @@ if(! function_exists('ibanner')){
 
 
         $banners = Banner::with(['categories']);
-        $banners->leftJoin('ibanner__banner__category', 'ibanner__banner__category.post_id', '=', 'ibanner__banners.id');
-        $banners->whereIn('ibanner__banner__category.category_id', [$id]);
+        $banners->leftJoin('ibanners__banner__category', 'ibanners__banner__category.banner_id', '=', 'ibanners__banners.id');
+        $banners->whereIn('ibanners__banner__category.category_id', [$id]);
         $banners->whereStatus(Status::PUBLISHED);
+
 
 
         $view = View::make($templates)
